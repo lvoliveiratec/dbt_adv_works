539afde789b913a08c0650ff5f1e5d957cd5a15b
Lucas Oliveira <lvoliveiratec@gmail.com>
2023-04-07 02:37:29 +0000

    alteração na fato

--- models/marts/comercial/fact_sap__detalhe_pedido_vendas.sql
+++ models/marts/comercial/fact_sap__detalhe_pedido_vendas.sql
@@ -116,6 +116,5 @@ with
         left join dim_produtos on joined.id_produto = dim_produtos.id_produto
         
     )
-select * --sum(preco_unitario)/count(data_envio) as ticket_medio
+select * 
 from gera_sk
--- group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
